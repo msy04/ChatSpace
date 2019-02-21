bdf2973e410371006696330b3b9aab47c4b203bb
msy04 <masaya@yamamotomasayanoMacBook-Pro.local>
2019-02-21 16:03:40 +0900

    Make auto-updating

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,6 @@
 $(function(){
   function buildSendMessageHTML(message){
-    var html = `<div class = "message">
+    var html = `<div class = "message" data-id = ${message.id}>
                   <div class = "upper-message">
                     <div class = "upper-message__user-name">
                       ${message.user_name}
@@ -16,6 +16,23 @@ $(function(){
                   </div>
                 </div>`
     return html;
+  }
+  function buildMessage(message){
+    var messages = $('.messages').append(`<div class = "message" data-id = ${message.id}>
+                                            <div class = "upper-message">
+                                              <div class = "upper-message__user-name">
+                                                ${message.user_name}
+                                              </div>
+                                              <div class="upper-message__date">
+                                                ${message.created_time}
+                                              </div>
+                                            </div>
+                                            <div class="lower-meesage">
+                                              <p class = "lower-message__content">
+                                                ${message.content}
+                                              </p>
+                                            </div>
+                                          </div>`);
   }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
@@ -38,4 +55,28 @@ $(function(){
       alert('error');
     })
   })
+
+  $(function(){
+    setInterval(update, 5000);
+  });
+  function update(){
+    if($('.message')[0]){
+      var message_id = $('.message:last').data('id');
+    } else {
+      var message_id = 0
+    }
+    $.ajax({
+      url: location.href,
+      type: 'GET',
+      data: {
+        message: { id: message_id }
+      },
+      dataType: 'json'
+    })
+    .always(function(data){
+      $.each(data, function(i, data){
+        buildMessage(data);
+      });
+    });
+  }
 })
---
+++ app/views/messages/index.json.jbuilder
@@ -0,0 +1,8 @@
+if @new_message.present?
+  json.array!(@new_message) do |message|
+    json.id message.id
+    json.user_name message.user.name
+    json.created_time message.created_at.strftime("%Y/%m/%d %H:%M")
+    json.content message.content
+  end
+end
